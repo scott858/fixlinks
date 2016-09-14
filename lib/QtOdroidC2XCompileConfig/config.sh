f3412ee511612329f7d9f51d046f8916fb286a9e
Scott Lee <scott.lee@nucleusscientific.com>
2016-09-14 16:43:58 -0400

    updated config.sh, who know why it started working

--- lib/QtOdroidC2XCompileConfig/config.sh
+++ lib/QtOdroidC2XCompileConfig/config.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-export rootfs=/opt/b2qt/2.0.1/sysroots/odroid-c2-mate
-export toolchain=/opt/toolchains/gcc-linaro-aarch64-linux-gnu-4.9-2014.09_linux/bin/aarch64-linux-gnu-
+export rootfs=/opt/sysroots/odroid-c2-mate
+#export toolchain=/opt/toolchains/gcc-linaro-aarch64-linux-gnu-4.9-2014.09_linux/bin/aarch64-linux-gnu-
+export toolchain=/opt/toolchains/gcc-linaro-5.3.1-2016.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 
 ./configure \
+ -v \
  -prefix /usr/local \
  -extprefix $rootfs/usr/local \
  -opensource \
@@ -31,8 +33,11 @@ export toolchain=/opt/toolchains/gcc-linaro-aarch64-linux-gnu-4.9-2014.09_linux/
  -device-option CROSS_COMPILE=$toolchain \
  -xplatform devices/linux-odroid-c2-g++ \
  -gstreamer 1.0 \
-# -no-use-gold-linker \
-# -v \
+ -no-use-gold-linker \
+# the config that worked didn't use gold linker though it wasn't explicitly 
+# turned off
 
-#  -xplatform devices/linux-odroid-c2-g++
-#/opt/toolchains/gcc-linaro-5.3.1-2016.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+#also I relinked the device filesystem directly after distro-upgrade and again
+#after install qt deps
+
+# -v \
